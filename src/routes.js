fc36ee906f5e25c6985ff58fa44739778b705251
Eduardo Santana <edusf0@hotmail.com>
2021-03-06 18:26:35 -0300

    atualizacao

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,9 @@ const routes = express.Router();
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
+routes.get('/users/:user_id/addresses', AddressController.index);
 routes.post('/users/:user_id/addresses', AddressController.store);
+
 
 
 module.exports = routes;
